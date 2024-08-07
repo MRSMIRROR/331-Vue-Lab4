d4195175c9830853351a3e6790aaa2c6cbeb9ac2
MRSMIRROR <hunsawut_h@cmu.ac.th>
2024-08-07 22:15:28 +0700

    question 3.5

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
 import nProgress from 'nprogress'
 import EventService from '@/services/EventService'
+import { useEventStore } from '@/stores/event'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -30,9 +31,11 @@ const router = createRouter({
       props: true,
       beforeEnter: (to) => {
         const id = parseInt(to.params.id as string)
+        const eventStore = useEventStore()
         return EventService.getEvent(id)
         .then((response) => {
           // need to setup the data for the event
+          eventStore.setEvent(response.data)
         }).catch((error) => {
           if (error.response && error.response.status === 404) {
             return {
